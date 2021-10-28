0aa5f93f784736e669430b4a8109ab504a221dfa
5n0weirdo <snoweirdo@protonmail.com>
2021-10-28 05:49:03 +0200

    Display container properly

--- src/Tour.js
+++ src/Tour.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price }) => {
   return (
-    <div>
-      <article className="single-tour"></article>
+    <article className="single-tour">
       <img src={image} alt={name} />
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
         </div>
       </footer>
-    </div>
+    </article>
   );
 };
 
