983c415eb179d1e715ac6e381401e755ef52e9c3
5n0weirdo <snoweirdo@protonmail.com>
2021-10-28 05:44:38 +0200

    Display name of tour

--- src/Tour.js
+++ src/Tour.js
@@ -4,7 +4,12 @@ const Tour = ({ id, name, info, image, price }) => {
   return (
     <div>
       <article className="single-tour"></article>
-      <img src={image} alt={image} />
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+        </div>
+      </footer>
     </div>
   );
 };
