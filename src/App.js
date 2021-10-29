cab22a7e7e46ec13acfcef04c39f248eb886e287
5n0weirdo <snoweirdo@protonmail.com>
2021-10-29 05:41:28 +0200

    Created a remove function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
     setLoading(true);
